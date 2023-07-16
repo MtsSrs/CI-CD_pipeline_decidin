b4b50e7f86d04ad3e53e7bd34f9add30b3f4b792
MtsSrs <matheus.1503@hotmail.com>
2023-07-16 16:24:41 -0300

    teste ci

--- test/mailers/app_mailer_test.rb
+++ test/mailers/app_mailer_test.rb
@@ -1,18 +1,17 @@
-# test/mailers/app_mailer_test.rb
-require 'rails_helper'
+require 'test_helper'
 
-RSpec.describe ApplicationMailer, type: :mailer do
-  describe 'sample_email' do
-    let(:mail) { ApplicationMailer.sample_email }
+class AppMailerTest < ActionMailer::TestCase
+  test 'sample email renders the headers' do
+    mail = ApplicationMailer.sample_email
 
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Subject')
-      expect(mail.to).to eq(['to@example.com'])
-      expect(mail.from).to eq(['from@example.com'])
-    end
+    assert_equal 'Subject', mail.subject
+    assert_equal ['to@example.com'], mail.to
+    assert_equal ['from@example.com'], mail.from
+  end
 
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hello')
-    end
+  test 'sample email renders the body' do
+    mail = ApplicationMailer.sample_email
+
+    assert_match 'Hello', mail.body.encoded
   end
 end
